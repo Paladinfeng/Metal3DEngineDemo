545051cf4f181521f38f4c51aae587927bc65af2
zang qilong <zangqilong@gmail.com>
2017-10-24 18:15:29 +0800

    Initial Commit

---
+++ Metal3DEngineDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Metal3DEngineDemo
+//
+//  Created by zang qilong on 2017/10/24.
+//  Copyright © 2017年 zang qilong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
